b53887863aad81e9a6fb50b65fbcd4610047489f
ChoiHyebin <91861082+ChoiHyebin@users.noreply.github.com>
2024-02-13 20:18:06 +0900

    [level 0] Title: 코드 처리하기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.py
@@ -0,0 +1,22 @@
+def solution(code):
+    answer = ''
+    mode = 0
+    
+    for idx in range(len(code)):
+        if(mode == 0):
+            if(code[idx] != "1"):
+                if(idx % 2 == 0):
+                    answer += code[idx]
+            elif(code[idx] == "1"):
+                mode = 1
+        elif(mode == 1):
+            if(code[idx] != "1"):
+                if(idx % 2 != 0):
+                    answer += code[idx]
+            elif(code[idx] == "1"):
+                mode = 0
+    
+    if(len(answer) == 0):
+        return "EMPTY"
+    
+    return answer
