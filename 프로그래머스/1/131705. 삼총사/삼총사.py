10ce2e09e7f79130f98e7fbda8508246b4d3b393
ChoiHyebin <91861082+ChoiHyebin@users.noreply.github.com>
2024-03-13 15:53:16 +0900

    [level 1] Title: 삼총사, Time: 0.04 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/131705. 삼총사/삼총사.py
@@ -0,0 +1,10 @@
+def solution(number):
+    res = 0
+    
+    for i in range(len(number)):
+        for j in range(i+1, len(number)):
+            for k in range(j+1, len(number)):
+                if number[i] + number[j] + number[k] == 0:
+                    res += 1
+    
+    return res
