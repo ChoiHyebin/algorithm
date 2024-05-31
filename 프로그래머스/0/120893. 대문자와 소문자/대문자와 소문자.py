19868b6866e2360eb919d21e2549826f765d9e90
ChoiHyebin <91861082+ChoiHyebin@users.noreply.github.com>
2024-05-31 10:14:48 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.28 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.py
@@ -0,0 +1,12 @@
+def solution(my_string):
+    answer = ''
+    
+    for i in my_string:
+        if i.islower():
+            i = i.upper()
+            answer += i
+        elif i.isupper():
+            i = i.lower()
+            answer += i
+            
+    return answer
