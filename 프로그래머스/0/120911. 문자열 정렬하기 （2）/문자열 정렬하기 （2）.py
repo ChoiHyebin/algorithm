4866c842354f8e9e5e88fce29ee0d1da190fed5a
ChoiHyebin <91861082+ChoiHyebin@users.noreply.github.com>
2024-09-06 10:42:54 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.py
@@ -0,0 +1,10 @@
+def solution(my_string):
+    answer = ''
+    
+    my_string = my_string.lower()
+    my_string = list(my_string)
+    my_string = sorted(my_string)
+    
+    answer = ''.join(my_string)
+    
+    return answer
