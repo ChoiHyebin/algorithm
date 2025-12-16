36f8d8c022e19ce3e9f74654769f125e0512c47b
ChoiHyebin <91861082+ChoiHyebin@users.noreply.github.com>
2025-12-16 16:24:06 +0900

    대표값

---
+++ 개인 공부/2-4 대표값.py
@@ -0,0 +1,19 @@
+n = int(input())
+scores = list(map(int, input().split()))
+
+avg = int(round(sum(scores) / n))
+
+min = 2147000000
+
+for idx, x in enumerate(scores):
+    tmp = abs(x-avg)
+    if tmp < min:
+        min = tmp
+        score = x
+        res = idx+1
+    elif tmp == min:
+        if x > score:
+            score = x
+            res = idx+1
+
+print(avg, res)
